f83a18611e506eb7626dab0e8ecece2ea9f166a4
Paul Staszko <paulstaszko@gmail.com>
2024-07-22 17:55:45 -0500

    quick

--- AHK-Notification-FSharp/Notify.fs
+++ AHK-Notification-FSharp/Notify.fs
@@ -8,33 +8,35 @@ module Notify =
     open Utils
     type Parameters =
         {
-            titleSize: int
-            titleColor: string
-            titleFont: string
-            messageSize: int
-            messageColor: string
-            logPath: string
-            messageFont: string
-            notificationTitle: string
-            notificationText: string
-            backgroundColor: string
-            padSize: int
-            ignoreHover: bool
+            TitleSize: int
+            TitleColor: string
+            TitleFont: string
+            MessageSize: int
+            MessageColor: string
+            LogPath: string
+            MessageFont: string
+            NotificationTitle: string
+            NotificationText: string
+            BackgroundColor: string
+            PadSize: int
+            IgnoreHover: bool
+            MaximumMessageLength: int
         }
         static member Default title message =
             {
-                titleSize = 30
-                titleColor = "7FA2CF"
-                logPath = ""
-                titleFont = "Segoe UI Light"
-                messageSize = 11
-                messageColor = "White"
-                messageFont = "Segoe UI"
-                backgroundColor = "2A2B2F"
-                padSize = 30
-                ignoreHover = false
-                notificationTitle = title
-                notificationText = message
+                TitleSize = 30
+                TitleColor = "7FA2CF"
+                LogPath = ""
+                TitleFont = "Segoe UI Light"
+                MessageSize = 11
+                MessageColor = "White"
+                MessageFont = "Segoe UI"
+                BackgroundColor = "2A2B2F"
+                PadSize = 30
+                IgnoreHover = false
+                NotificationTitle = title
+                NotificationText = message
+                MaximumMessageLength = 100
             }
     (*
 
@@ -62,32 +64,33 @@ ignoreHover := 0
 	logPath (save a record of notification calls)
 	padSize
     *)
-    let private log exePath args =
-        let exe = System.IO.Path.GetFullPath(exePath)
-        let logMessage = $@"{exe} {args}"
-        //System.IO.File.AppendAllText(logPath, logMessage + Environment.NewLine) |> ignore
-        let q = logMessage
-        ()
+
+    //let private log exePath args =
+    //    let exe = System.IO.Path.GetFullPath(exePath)
+    //    let logMessage = $@"{exe} {args}"
+    //    //System.IO.File.AppendAllText(logPath, logMessage + Environment.NewLine) |> ignore
+    //    let q = logMessage
+    //    ()
+
     let Notify (parameters: Parameters) =
         let exePath = @"Notification.exe"
         if System.IO.File.Exists(exePath) then
-            //let title = parameters.notificationTitle.Replace("\\", "\\\\")
-            let message = parameters.notificationText.Replace("\\", "\\\\").Substring(0, Math.Min(parameters.notificationText.Length, 100))
+            let message = parameters.NotificationText.Replace("\\", "\\\\").Substring(0, Math.Min(parameters.NotificationText.Length, parameters.MaximumMessageLength))
             let args =
                 [
                    //Op: Auto
                    "notificationText" , message
-                   "notificationTitle", parameters.notificationTitle
-                   "logFile"          , parameters.logPath
-                   "backgroundColor"  , parameters.backgroundColor
-                   "padSize"          , parameters.padSize |> str
-                   "titleSize"        , parameters.titleSize |> str
-                   "titleColor"       , parameters.titleColor
-                   "titleFont"        , parameters.titleFont
-                   "messageSize"      , parameters.messageSize |> str
-                   "messageColor"     , parameters.messageColor
-                   "messageFont"      , parameters.messageFont
-                   "ignoreHover"      , if parameters.ignoreHover then "1" else "0"
+                   "notificationTitle", parameters.NotificationTitle
+                   "logFile"          , parameters.LogPath
+                   "backgroundColor"  , parameters.BackgroundColor
+                   "padSize"          , parameters.PadSize |> str
+                   "titleSize"        , parameters.TitleSize |> str
+                   "titleColor"       , parameters.TitleColor
+                   "titleFont"        , parameters.TitleFont
+                   "messageSize"      , parameters.MessageSize |> str
+                   "messageColor"     , parameters.MessageColor
+                   "messageFont"      , parameters.MessageFont
+                   "ignoreHover"      , if parameters.IgnoreHover then "1" else "0"
                    //Op: End
                 ]
                 |> Seq.filter (snd >> isNotEmpty)
@@ -96,7 +99,7 @@ ignoreHover := 0
                     $@"""%s{k}=%s{v}""")
                 |> String.concat " "
                 //|> fun x -> $@"""%s{exePath}"" {x}"
-            log exePath args
+            //log exePath args
             System.Diagnostics.Process.Start(exePath, args) |> ignore
         else
             failwithf "%s doesn't exist." exePath
